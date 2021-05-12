3cc08bb63dfde472490b0d6cc94d622fbaf8a3ce
zuevval <valera.zuev.zva@gmail.com>
2021-05-12 17:59:18 +0300

    add tiles recognition

--- angela_braille_bot.py
+++ angela_braille_bot.py
@@ -1,4 +1,7 @@
 import os
+import sys
+from pathlib import Path
+from subprocess import Popen
 
 import telebot
 
@@ -12,6 +15,25 @@ def send_welcome(message):
                                      "I will do Braille character recognition from photo "
                                      "using I\. Ovodov's [Angelina Braille Reader](http://angelina-reader.ru/) "
                                      "as a backend\.")), parse_mode="MarkdownV2")
+
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    tmp_dir = Path("tmp")
+    tmp_dir.mkdir(exist_ok=True)
+    img_input_filename = "image.jpg"
+
+    with open(tmp_dir / img_input_filename, "wb") as new_file:
+        new_file.write(downloaded_file)
+
+    Popen([sys.executable, "tiles-recognition/src/main.py", "-vv", tmp_dir])
+
+    with open(Path("out") / "result-image.png", "rb") as img:
+        bot.send_photo(message.chat.id, img)
 
 
 if __name__ == "__main__":
